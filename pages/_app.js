a6ae04bc48ecb4242c1ea4fbdf7a68868c6f8424
ice-bear <andydowsen47us@gmail.com>
2022-08-20 14:48:18 +0530

    Navbar commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import NavbarComponent from "./Router/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <NavbarComponent />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
