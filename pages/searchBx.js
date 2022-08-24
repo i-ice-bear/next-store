37c2bd731b0ac19f5a8cf3276d06202105800c0b
ice-bear <andydowsen47us@gmail.com>
2022-08-24 19:17:07 +0530

    Algolia search box

---
+++ pages/searchBx.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBoxComponent = () => {
+  return (
+    <div className='my-40'>
+        SearchBoxComponent
+    </div>
+  )
+}
+
+export default SearchBoxComponent
---
+++ scripts/build-search.js
@@ -0,0 +1,46 @@
+// build-search.js
+const dotenv = require("dotenv");
+const algoliasearch = require("algoliasearch/lite");
+
+async function getAllBlogPosts() {
+  // write your code to fetch your data
+}
+
+function transformPostsToSearchObjects(posts) {
+  const transformed = posts.map((post) => {
+    return {
+      objectID: post.sys.id,
+      firstname: post.firstname,
+      lastname: post.lastname,
+      slug: post.slug,
+      zipcode: post.zipcode,
+    };
+  });
+}
+
+(async function () {
+  dotenv.config();
+
+  try {
+    const posts = await getAllBlogPosts();
+    const transformed = transformPostsToSearchObjects(posts);
+
+    const client = algoliasearch(
+      process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+      process.env.ALGOLIA_SEARCH_ADMIN_KEY
+    );
+
+    const index = client.initIndex("my_awesome_content");
+    const algoliaResponse = await index.saveObjects(transformed);
+
+    console.log(
+      `🎉 Sucessfully added ${
+        algoliaResponse.objectIDs.length
+      } records to Algolia search. Object IDs:\n${algoliaResponse.objectIDs.join(
+        "\n"
+      )}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+})();
