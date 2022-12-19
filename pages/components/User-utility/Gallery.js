b9f6799400f631589c4c38717b7ebfbeebd16306
ice-bear <andydowsen47us@gmail.com>
2022-12-19 15:12:41 +0530

    Fetching images from api: in progress

--- pages/components/User-utility/Gallery.js
+++ pages/components/User-utility/Gallery.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
+import axios from "axios";
 
 const GalleryComponent = () => {
-  const url = "https://api.unsplash.com/photos/?client_id=b4CuH4vAeA99oEm0tAW7SyxH6rcjpUVB1lp06kLnmqo"
+  const [imageFetch, setImageFetch] = React.useState([]);
+
+  const __fetch__image = async () => {
+    const axios__get = await axios.get(
+      "https://api.unsplash.com/photos/?client_id=b4CuH4vAeA99oEm0tAW7SyxH6rcjpUVB1lp06kLnmqo"
+    );
+    setImageFetch(axios__get.data.articles);
+  };
+
+  React.useEffect(() => {
+    __fetch__image();
+  }, []);
   return (
     <>
       <section className="overflow-hidden">
         <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
           <div className="flex flex-wrap -m-1 md:-m-2">
             <div className="flex flex-wrap w-1/2">
-              <div className="w-1/2 p-1 md:p-2">
+              {imageFetch &&
+                imageFetch.map((items) => {
+                  return (
+                    <div className="w-1/2 p-1 md:p-2" key={items.author}>
+                      <img src={items.urls.full} alt="something" />
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default GalleryComponent;
+
+{
+  /* <div className="w-1/2 p-1 md:p-2">
                 <img
                   alt="gallery"
                   className="block object-cover object-center w-full h-full rounded-lg"
@@ -52,13 +83,5 @@ const GalleryComponent = () => {
                   className="block object-cover object-center w-full h-full rounded-lg"
                   src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
                 />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
-
-export default GalleryComponent;
+              </div> */
+}
